091c9038e4eea6b3c8f5beddd153f6d2ceaa8997
Julia Rosa <juliarosa040108@gmail.com>
2025-04-15 08:33:57 -0300

    primeiro commit

---
+++ localadmin/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const cpfInput = document.getElementById('cpf');
+
+  cpfInput.addEventListener('input', function () {
+    let value = cpfInput.value.replace(/\D/g, '');
+
+    if (value.length > 11) value = value.slice(0, 11);
+
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+    cpfInput.value = value;
+  });
+});
